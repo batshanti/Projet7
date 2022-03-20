f3ac00804025879472fdad3316d8b3fae5883cb2
batshanti <teddylouvet@yahoo.fr>
2022-03-20 21:58:54 +0100

    add function load_csv

--- bruteforce.py
+++ bruteforce.py
@@ -6,5 +6,11 @@ import time
 
 
 def load_csv():
-    with open("database.csv") as csvfile:
-        
+    with open("invest.csv") as csvfile:
+        file = csv.reader(csvfile, delimiter=',')
+        file_list = []
+        for line in file:
+            file_list.append(line[0], float.(line[1]), float.(line[2]))
+
+        return file_list
+       
