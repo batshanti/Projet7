40ea862dd007832832f487badad57e7538c1c99e
batshanti <teddylouvet@yahoo.fr>
2022-03-20 22:20:00 +0100

    minor modification in bruteforce.py

--- bruteforce.py
+++ bruteforce.py
@@ -3,6 +3,12 @@ import time
 
 
 
+def main():
+    file_list = load_csv()
+    # print(file_list)
+    print(file_list[0])
+    print(file_list[1])
+
 
 
 def load_csv():
@@ -10,7 +16,10 @@ def load_csv():
         file = csv.reader(csvfile, delimiter=',')
         file_list = []
         for line in file:
-            file_list.append(line[0], float.(line[1]), float.(line[2]))
+            file_list.append([line[0], float(line[1]), float(line[2])])
 
         return file_list
-       
+
+
+if __name__ == "__main__":
+    main()
