d68889ab3306652b0f97a6c2490887b4f187a5ba
LOUVET Teddy (Prestataire) <ex.teddy.louvet@dom-idf.justice.fr>
2022-03-25 13:01:17 +0100

    add function actions_combo() + calculate_invest() + calculate_gain()

--- bruteforce.py
+++ bruteforce.py
@@ -1,14 +1,16 @@
+from itertools import combinations
+
 import csv
 import time
 
 
 
+MAX = 500
+
 def main():
     file_list = load_csv()
-    # print(file_list)
-    print(file_list[0])
-    print(file_list[1])
-
+    best_invest_list = actions_combo(file_list)
+    print(best_invest_list)
 
 
 def load_csv():
@@ -19,6 +21,47 @@ def load_csv():
             file_list.append([line[0], float(line[1]), float(line[2])])
 
         return file_list
+
+
+def actions_combo(list):
+
+    best_invest_list = []
+    total_gain = 0
+
+    for line in range(len(list)):
+        combination = combinations(list, line + 1)
+
+        for combi in combination:
+            total_price = calculate_invest(combi)
+            
+            if total_price <= MAX:
+                gain = calculate_gain(combi)
+            
+                if gain > total_gain:
+                    total_gain = gain
+                    best_invest_list = combi
+
+    return best_invest_list 
+
+
+def calculate_invest(combi):
+    cost_actions = []
+    
+    for price in combi:
+        cost_actions.append(price[1])
+
+    total_cost = sum(cost_actions)    
+    return total_cost
+
+
+def calculate_gain(combi):
+    gain = []
+
+    for line in combi:
+        gain.append(line[1] * line[2] / 100)
+
+    total_gain = sum(gain)
+    return total_gain
 
 
 if __name__ == "__main__":
