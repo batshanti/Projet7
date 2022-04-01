a6e2e016a3868332eddd8e343c64960ab7900ecc
LOUVET Teddy (Prestataire) <ex.teddy.louvet@dom-idf.justice.fr>
2022-04-01 16:49:25 +0200

    add algo sac à dos

--- opti.py
+++ opti.py
@@ -8,7 +8,7 @@ MAX = 500
 
 def main():
     file_list = load_csv()
-    invest_dynamique(file_list)
+    invest_dynamique(file_list, MAX)
 
 
 def load_csv():
@@ -21,30 +21,33 @@ def load_csv():
         return file_list
 
 
-def invest_dynamique(file_list):
-    
+def invest_dynamique(file_list, inv_max):
 
 
-    matrice = [[0 for x in range(MAX + 1)] for x in range(len(file_list) + 1)]
+    matrice = [[0 for x in range(inv_max + 1)] for x in range(len(file_list) + 1)]
 
     for i in range(1, len(file_list) + 1):
-        for w in range(1, MAX + 1):
+        for w in range(1, inv_max + 1):
 
             if file_list[i-1][1] <= w:
                 a = file_list[i-1][1]
-                print(type(a))
-                matrice[i][w] = max(file_list[i-1][2] + matrice[i-1][w-a], matrice[i-1][w])
+                matrice[i][w] = max(file_list[i-1][2] + matrice[i-1][w-int(file_list[i-1][1])], matrice[i-1][w])
             else:
                 matrice[i][w] = matrice[i-1][w]
-    print(matrice)
 
+    actions_selection = []
+    nb_element = len(file_list)
 
+    while inv_max >= 0 and nb_element >= 0:
+        action = file_list[nb_element-1]
 
+        if matrice[nb_element][inv_max] == matrice[nb_element-1][inv_max-int(action[1])] + int(action[2]):
+            actions_selection.append(action)
+            inv_max -= int(action[1])
+        nb_element -= 1
 
-
-
-
-
+      
+    print(list(reversed(actions_selection)))
 
 
 if __name__ == "__main__":
