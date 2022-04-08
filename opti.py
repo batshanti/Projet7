c21c606f4012ca8472391071363c4e1d3b1b2995
LOUVET Teddy (Prestataire) <ex.teddy.louvet@dom-idf.justice.fr>
2022-04-08 15:46:47 +0200

    fix opti.py

--- opti.py
+++ opti.py
@@ -1,5 +1,3 @@
-from itertools import combinations
-
 import csv
 import time
 
@@ -16,64 +14,59 @@ def load_csv():
         file = csv.reader(csvfile, delimiter=',')
         file_list = []
         for line in file:
-            file_list.append([line[0], float(line[1]), float(line[2])])
+            file_list.append([line[0],
+             float(line[1]),
+             (float(line[1])*float(line[2])/100)])
 
         return file_list
 
 
 def invest_dynamique(file_list, inv_max):
 
+    actions_selection = []
+    nb_actions = len(file_list)
+    cost = []
+    gain = []
 
-    matrice = [[0 for x in range(inv_max + 1)] for x in range(len(file_list) + 1)]
+    for action in file_list:
+        cost.append(int(action[1]))
+        gain.append(int(action[2]))
 
-    for i in range(1, len(file_list) + 1):
+
+    matrice = [[0 for x in range(inv_max + 1)] for x in range(nb_actions + 1)]
+
+    for i in range(1, nb_actions + 1):
         for w in range(1, inv_max + 1):
 
-            if file_list[i-1][1] <= w:
-                a = file_list[i-1][1]
-                matrice[i][w] = max(file_list[i-1][2] + matrice[i-1][w-int(file_list[i-1][1])], matrice[i-1][w])
+            if cost[i-1] <= w:
+                matrice[i][w] = max(gain[i-1] + matrice[i-1][w-int(cost[i-1])], matrice[i-1][w])
             else:
                 matrice[i][w] = matrice[i-1][w]
 
-    actions_selection = []
-    nb_actions = len(file_list)
 
     while inv_max >= 0 and nb_actions >= 0:
-        action = file_list[nb_actions-1]
-
-        if matrice[nb_actions][inv_max] == matrice[nb_actions-1][inv_max-int(action[1])] + int(action[2]):
-            actions_selection.append(action)
-            inv_max -= int(action[1])
+        
+        if matrice[nb_actions][inv_max] == matrice[nb_actions-1][inv_max-int(cost[nb_actions-1])] + int(gain[nb_actions-1]):
+            actions_selection.append(file_list[nb_actions-1])
+            inv_max -= cost[nb_actions-1]
         nb_actions -= 1
-
-      
+ 
     return list(reversed(actions_selection))
-
-
-def calculate_gain(invest_list):
-    gain = []
-
-    for line in invest_list:
-        gain.append(line[1] * line[2] / 100)
-
-    total_gain = sum(gain)
-    return total_gain
-
 
 def display(invest_list):
     actions_name = []
     cost = []
-    
+    gain = []
+
     for action in invest_list:
         actions_name.append(action[0])
         cost.append(action[1])
+        gain.append(action[2])
 
     print("List actions boughts : " + str(actions_name))
     print("Cost : " + str(sum(cost)) + " Euros")
-    print("Profit : " + str(calculate_gain(invest_list)) + " Euros\n")
-
+    print("Profit : " + str(sum(gain)) + " Euros\n")
 
 
 if __name__ == "__main__":
     main()
-
