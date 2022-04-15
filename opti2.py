721fe4cd25faa739041ee85481300283f6d98a76
LOUVET Teddy (Prestataire) <ex.teddy.louvet@dom-idf.justice.fr>
2022-04-15 11:30:28 +0200

    add menu choose dataset

--- opti2.py
+++ opti2.py
@@ -1,18 +1,39 @@
 import csv
 import time
 
-start = time.time()
 
 MAX = 500
+DATASET = [
+
+    "1 : invest.csv",
+    "2 : dataset1.csv",
+    "3 : dataset2.csv"
+]
+
 
 def main():
-    file_list = load_csv()
+    dataset = choose_dataset()
+    global start
+    start = time.time()
+    file_list = load_csv(dataset)
     sort_list = sorted_actions(file_list)
     invest_list = find_invest(sort_list, MAX)
     display(invest_list)
 
-def load_csv():
-    with open("dataset2.csv") as csvfile:
+def choose_dataset():
+    choices = {
+        "1" : "invest.csv",
+        "2" : "dataset1.csv",
+        "3" : "dataset2.csv"
+    }
+
+    for line in DATASET:
+        print(line)
+    choice = input("Choose dataset : ")
+    return choices[choice]
+
+def load_csv(dataset):
+    with open(dataset) as csvfile:
         file = csv.reader(csvfile, delimiter=',')
         file_list = []
         for line in file:
@@ -24,24 +45,23 @@ def load_csv():
         return file_list
 
 def sorted_actions(invest_list):
-	return sorted(invest_list, key=lambda action: action[2], reverse=True)
+    return sorted(invest_list, key=lambda action: action[2], reverse=True)
 
 
 def find_invest(invest_list, max):
-	best_invest = []
-	total_cost = 0
-	i = 0
+    best_invest = []
+    total_cost = 0
+    i = 0
 
-	while i < len(invest_list) and total_cost <= max:
-		action = invest_list[i]
-		action_cost = action[1]
-		if total_cost + action_cost <= max:
-			best_invest.append(action)
-			total_cost += action_cost
-		i += 1	
+    while i < len(invest_list) and total_cost <= max:
+        action = invest_list[i]
+        action_cost = action[1]
+        if total_cost + action_cost <= max:
+            best_invest.append(action)
+            total_cost += action_cost
+        i += 1  
 
-	return best_invest
-		
+    return best_invest
 
 def display(invest_list):
     actions_name = []
@@ -56,7 +76,7 @@ def display(invest_list):
     print("List actions boughts : ")
 
     for action in actions_name:
-    	print(action)
+        print(action)
 
     print("Total Cost : " + str(sum(cost)) + " Euros")
     print("Profit : " + str(sum(gain)) + " Euros")
